0de7705cacb8459e0d93e0dead000af5f9588fa7
amit <amit.jt.a105@gmail.com>
2023-04-13 17:56:03 +0530

    13th

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {View} from 'react-native'
+
+export default App=()=>{
+  <View style={{
+    height:50,
+    width:50,
+    backgroundColor:'red'
+  }}>
+
+  </View>
+}
